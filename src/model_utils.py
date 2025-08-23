9b7bc578f34c2527a6cc4219b06b083f7a2bda4e
Ashmiit007 <96116509+Ashmiit007@users.noreply.github.com>
2025-08-24 00:25:17 +0530

    Update model_utils.py

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,1 +1,29 @@
 
+import torch
+import torch.nn as nn
+from torchprofile import profile_macs
+from .config import DEVICE
+
+def load_model(path):
+    return torch.load(path, map_location=DEVICE)
+
+def find_accuracy(loader, model, device=DEVICE):
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    model.train()
+    return 100 * correct / total
+
+def calculate_flops(model, input_shape, device=DEVICE):
+    model.eval()
+    with torch.no_grad():
+        dummy_input = torch.randn(input_shape).to(device)
+        flops = profile_macs(model, dummy_input)
+    model.train()
+    return flops
