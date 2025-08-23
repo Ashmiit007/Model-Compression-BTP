ed96615e2bc19aef0221aaffef42a444c2107c9a
Ashmiit007 <96116509+Ashmiit007@users.noreply.github.com>
2025-08-24 00:34:21 +0530

    Update run_intel.py

--- scripts/run_intel.py
+++ scripts/run_intel.py
@@ -1,1 +1,15 @@
 
+from src.datasets import get_intel_loaders
+from src.model_utils import load_model, find_accuracy
+from src.config import MODEL_PATH
+from src.ppo_conv import ppo_conv
+from src.ppo_fc import ppo_fc
+
+if __name__ == "__main__":
+    train_loader, test_loader, classes = get_intel_loaders()
+    model = load_model(f"{MODEL_PATH}/base_alexnet_intel.pth")
+    acc = find_accuracy(test_loader, model)
+    print(f"Initial Accuracy: {acc:.2f}%")
+
+    # Run PPO conv & FC iterations here
+    # Save weights
