23ae710ad172d67c7161ecb0f4019ea90d04558b
Ashmiit007 <96116509+Ashmiit007@users.noreply.github.com>
2025-08-24 00:13:51 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,14 @@
+import torch
+
+# General
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+BATCH_SIZE = 128
+NUM_EPOCHS = 40
+LEARNING_RATE = 0.001
+
+# Paths
+DATA_PATH = "./data"
+INTEL_PATH = "./data/intel-image-classification"
+MODEL_PATH = "./models"
+SAVED_WEIGHTS = "./models/saved_weights"
 
