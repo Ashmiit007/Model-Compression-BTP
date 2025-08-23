a04d65811a4e288dd83a7169cea930c6326afa54
Ashmiit007 <96116509+Ashmiit007@users.noreply.github.com>
2025-08-24 00:31:54 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
 
+import math
+
+def sigmoid(x):
+    if x > -70:
+        return 1 / (1 + math.exp(-x))
+    return 0
+
+def update_w(delta_w, weights, lr=0.01):
+    for l, layer in enumerate(weights):
+        for i in range(len(layer)):
+            weights[l][i] -= lr * delta_w[l][i]
+    return weights
+
+def calculate_advantage(layer_reward, total_layer_reward, iter_cnt):
+    return [layer_reward[i] - (total_layer_reward[i] / iter_cnt) for i in range(len(layer_reward))]
